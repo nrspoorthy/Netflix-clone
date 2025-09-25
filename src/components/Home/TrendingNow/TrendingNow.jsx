d9b50e04564353174aad895f6b079279ead6cc87
nrspoorthy <nrspoorthy@gmail.com>
2025-09-25 12:09:15 +0530

    Fix trending now CSS import for Vercel build

--- src/components/Home/TrendingNow/TrendingNow.jsx
+++ src/components/Home/TrendingNow/TrendingNow.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import './trendingnow.css';
+import './Trendingnow.css';
 import TrendingNowDetails from './TrendingNowDetails';
 
 const TrendingNow = () => {
